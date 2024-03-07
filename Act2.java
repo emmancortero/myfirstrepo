6c1187655e123e0db0bbd67bdef33f7d387e7975
emmancortero <162523273+emmancortero@users.noreply.github.com>
2024-03-07 09:54:47 +0800

    Add files via upload

---
+++ Act2.java
@@ -0,0 +1,25 @@
+public class Act2{
+
+public static void main(String [] args){
+	
+int num1 = 1;
+byte num2 = 2;
+short num3 = 3;
+long num4 = 4;
+float num5 = 5;
+double num6 = 6;
+String name = "Paul";
+boolean result = true;
+char gender = 'M';
+
+System.out.println("The Value of int is=" + num1);
+System.out.println("The Value of byte is=" + num2);
+System.out.println("The Value of short is=" + num3);
+System.out.println("The Value of long is=" + num4);
+System.out.println("The Value of float is=" + num5);
+System.out.println("The Value of double is=" + num6);
+System.out.println("The Value of string is=" + name);
+System.out.println("The Value of boolean is=" + result);
+System.out.println("The Value of char is=" + gender);
+}
+}
